60262cdd6393683d1a751a3708c10e76770a6a5b
Anthony Cornehl <accornehl@gmail.com>
2013-11-30 13:44:11 -0800

    add perl repl one-liner

--- .aliases/perl.sh
+++ .aliases/perl.sh
@@ -6,3 +6,6 @@ pod.w () {
 alias perldb="perl -de0"
 alias pod="perldoc"
 alias podf="perldoc -f"
+prep () {
+    perl -MData::Dumper -MTerm::ReadLine -e '$r = Term::ReadLine->new(1); while(defined($_ = $r->readline("> "))){$ret=Dumper(eval($_)); $err=$@; if($err ne ""){print $err;}else{print $ret;}}';
+}
